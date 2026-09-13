11e7013940a2972648e9c0ec82732c8a78abb689
AngelyDema <angelydema@gmail.com>
2026-09-12 22:18:41 -0500

    "Variables y autorización para apagar la máquina

--- main.tf
+++ main.tf
@@ -46,3 +46,34 @@ resource "google_compute_instance" "web" {
 
   metadata_startup_script = file("arranque.sh")
 }
+
+
+EVIDENCIA DEL APPLY: 
+angelysofiapg@cloudshell:~/PRACTICA2 (nube-practica-1-507220)$  terraform apply -var tipo_maquina=e2-small
+google_compute_firewall.permitir_http: Refreshing state... [id=projects/nube-practica-1-507220/global/firewalls/permitir-http]
+google_compute_instance.web: Refreshing state... [id=projects/nube-practica-1-507220/zones/us-central1-a/instances/web-tf]
+
+Changes to Outputs:
+  ~ ip_externa = "34.71.41.101" -> "34.71.75.158"
+
+You can apply this plan to save these new output values to the Terraform state, without changing any real infrastructure.
+
+Do you want to perform these actions?
+  Terraform will perform the actions described above.
+  Only 'yes' will be accepted to approve.
+
+  Enter a value: yes
+
+
+Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
+
+Outputs:
+
+ip_externa = "34.71.75.158"
+
+EVIDENCIA IP:
+
+angelysofiapg@cloudshell:~/PRACTICA2 (nube-practica-1-507220)$  terraform output ip_externa
+"34.71.75.158"
+
+
